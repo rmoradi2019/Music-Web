88a5343a414709471036e302b98c22904ea31d92
Reza Moradi <54495069+rmoradi2019@users.noreply.github.com>
2023-03-27 19:40:00 +0330

    Player Online

---
+++ index.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="رادیو آنلاین سورنا فایل">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>رادیو آنلاین و موزیک سورنا</title>
+    <link rel="icon" type="image/x-icon" sizes="32x32" href="favicon.ico">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+
+<div class="row">
+  <div class="column">
+    <div class="card">رادیو ارم
+</br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://37.59.47.192:8200/listning.mp3" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+
+</div>
+    </div> 
+
+  <div class="column">
+    <div class="card">رادیو تکنوازان
+    </br>
+</br>
+<div class="audio">
+       <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://107.181.227.250:8244/stream" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">رادیو صدای ایران
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://lyssna.fnf.nu:8080/gnf102.mp3" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">رادیو فاز
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://www.radiofaaz.com:8000/radiofaaz" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+  </div>
+
+
+<div class="row">
+  <div class="column">
+    <div class="card">رادیو فردا
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="https://n0d.radiojar.com/cp13r2cpn3quv?rj-ttl=5&rj-tok=AAABfo5IiXkAPR8eKqpyd9hwlw" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">رادیو همسفر
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://streamingv2.shoutcast.com/Hamsafarradio?lang=en-US%2cen%3bq%3d0.5" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">رادیو ایران اینترنشنال
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://n11.radiojar.com/dfnrphnr5f0uv?rj-ttl=5&rj-tok=AAABheinLc8A87PKFGNJUX9rCA" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+  
+    <div class="column">
+    <div class="card">رادیو قلب لندن 
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://ice-sov.musicradio.com/HeartLondonMP3" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="row">
+      <div class="column">
+    <div class="card">رادیو فاما ریمیکس  
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://nxts.ir:39210/Fama-Remix.mp3" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+  
+        <div class="column">
+    <div class="card">رادیو پوتک   
+    </br>
+</br>
+<div class="audio">
+       <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://nxts.ir:39210/Pouria-Putak.mp3" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+  
+          <div class="column">
+    <div class="card">رادیو دوالیپا   
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://nxts.ir:39210/duaLipa.mp3" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+</div>
+    </div>
+
+  
+            <div class="column">
+    <div class="card">رادیو زد بازی   
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://nxts.ir:39210/ZedBazi.mp3" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+              <div class="column">
+    <div class="card">رادیو ترکی    
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://nxts.ir:39210/Turkish.mp3" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+  
+                <div class="column">
+    <div class="card">رادیو روسی    
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://nxts.ir:39210/Russian.mp3" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+
+                  <div class="column">
+    <div class="card">رادیو جوان    
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://nxts.ir:39210/RadioJavan.mp3" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+  
+                    <div class="column">
+    <div class="card">رادیو ساب موزیک    
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://nxts.ir:39210/SubMusic.mp3" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+  
+  
+</div>
+
+<div class="row">
+                   <div class="column">
+    <div class="card">رادیو تی ام بکس     
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://nxts.ir:39210/TmBax.mp3" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+  
+                     <div class="column">
+    <div class="card">رادیو ریمیکس انگلیسی       
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://nxts.ir:39210/English-Remix.mp3" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+  
+                       <div class="column">
+    <div class="card">رادیو انگلیسی        
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://nxts.ir:39210/English.mp3" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+  
+                         <div class="column">
+    <div class="card">رادیو رپ        
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://nxts.ir:39210/Rap.mp3" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+
+</div>
+
+<div class="row">
+
+                         <div class="column">
+    <div class="card">رادیو هشت بعدی        
+    </br>
+</br>
+<div class="audio">
+        <audio controls preload="none" style="border-radius: 50px; background-color: blue;">
+  <source src="http://nxts.ir:39210/8D.mp3" type="audio/mpeg">
+مرورگر شما این فرمت را پشتیبانی نمیکند</audio>
+</div>
+    </div>
+  </div>
+  
+                           <div class="column">
+    <div class="card">پلی لیست گلچین          
+    </br>
+</br>
+<div class="audio">
+        <a href="https://dl.sorenafile.ir/music/"><button class="btn">ورود</button></a>
+</div>
+    </div>
+  </div>
+
+</div>
+
+<!-- Statistics by www.1abzar.com --->
+<script type="text/javascript" src="https://1abzar.ir/abzar/tools/stat/amar-v3.php?color=F5F5F5&bg=1A1B2E&kc=888888&kadr=1&amar=adbus51zvv5ogcipbi10-zxegozx0h&show=1|0|1|0|0|1|1"></script><div style="display:none"><h3><a href="https://www.1abzar.com/abzar/stat.php">&#1570;&#1605;&#1575;&#1585;&#1711;&#1740;&#1585; &#1608;&#1576;&#1604;&#1575;&#1711;</a></h3></div>
+<!-- Statistics by www.1abzar.com --->
+
+
+</body>
+</html>
